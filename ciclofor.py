13b975005fc16ec555e450f994dd7f62afe5c641
carlosfadul <carlosfadul@gmail.com>
2026-03-09 22:00:43 -0500

    Segundo evío

---
+++ ciclofor.py
@@ -0,0 +1,17 @@
+"""
+for x in range(1, 11):
+    print(x)
+
+for x in range(5):
+    print(x)
+
+for x in range(1, 11, 2):
+    print(x)
+
+
+texto = "Python es muy bacano"
+for letra in texto:
+    print(letra)
+"""
+for x in range(1, 11):
+    print(x)
