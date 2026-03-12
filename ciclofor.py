c3906fe20f85831923ab210ba808d287af448ae0
carlosfadul <carlosfadul@gmail.com>
2026-03-11 20:37:33 -0500

    incluye las tablas de multiplicar

--- ciclofor.py
+++ ciclofor.py
@@ -13,5 +13,13 @@ texto = "Python es muy bacano"
 for letra in texto:
     print(letra)
 """
+"""
+3x1=3
+3x2=6
+3x3=9
+3x4=12
+"""
+tabla = int(input("Ingrese la tabla de multiplicar que desea: "))
 for x in range(1, 11):
-    print(x)
+    print(tabla, "x", x, "=",  x*tabla)
+print("hola")
